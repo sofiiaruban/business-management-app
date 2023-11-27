ec351b165011b3b46b9321bbd8e65a52de624762
Sofia Ruban <sophiaruban249324@gmail.com>
2023-11-27 20:49:19 +0200

    chore: Add project file structure

---
+++ client/src/pages/Auth.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+export const Auth: FC = () => {
+  return (
+    <div>Auth</div>
+  )
+}
---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+export const Layout:FC = () => {
+  return (
+    <div>Layout</div>
+  )
+}
