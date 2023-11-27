18739ac3d2ea06e8ed62bb2994caf862b835a598
Sofia Ruban <sophiaruban249324@gmail.com>
2023-11-27 20:28:34 +0200

    chore: Configure husky, prettier, eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,16 +3,25 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react-refresh', 'react', 'react-hooks', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
+      { allowConstantExport: true }
+    ]
+  }
 }
