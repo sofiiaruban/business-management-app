633fafceebb5c2b4a04e64f6f71db62768b1e764
Sofia Ruban <sophiaruban249324@gmail.com>
2023-11-28 12:19:46 +0200

    feat(auth): Add local storage helpers

---
+++ client/src/helpers/localstorage.helper.ts
@@ -0,0 +1,12 @@
+export function getTokenFromLocalStorage():void {
+  const data = localStorage.getItem('token')
+  const token = data ? JSON.parse(data) : ''
+
+  return token
+}
+export function setTokenToLocalStorage(key: string, token:string):void {
+  localStorage.setItem(key, JSON.stringify(token))
+}
+export function removeTokenFromLocalStorage(key: string): void {
+  localStorage.removeItem(key);
+}
